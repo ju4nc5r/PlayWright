f5c92cea24f6af44ce478386f6ccfed313a387d6
AST14698 <matias.astrada@macrobma.com.ar>
2024-05-27 17:55:21 -0300

    Se agregan scripts

--- configs/git.sh
+++ configs/git.sh
@@ -1,30 +1,25 @@
-#!/bin/bash
+# Accede a los parámetros pasados desde Jenkins
+user=$1
+pass=$2
+DIR="$PWD"
+FOLDER='sap-crm-tests'
+FULL_PATH="$DIR/$FOLDER"
 
-# Directorio que se va a monitorear
-WATCH_DIR="/home/jenkins/workspace"
+echo "${DIR}"
+echo "${FULL_PATH}"
 
-# Archivo que se va a copiar
-git="/home/jenkins/workspace/git.sh"
-post_results="/home/jenkins/workspace/send_results.sh"
-
-# Función que se ejecuta cuando se detecta una nueva carpeta
-copy_to_new_dir() {
-    local new_dir="$1"
-    echo "Nueva carpeta detectada: $new_dir"
-    cp -r "$git" "$new_dir"
-    echo "Carpeta copiada a $new_dir"
-}
-
-# Monitorear el directorio en busca de nuevas carpetas
-inotifywait -m -e create --format '%w%f' "$WATCH_DIR" | while read NEW_PATH
-do
-    # Verificar si la nueva ruta es un directorio
-    if [ -d "$NEW_PATH" ]; then
-        copy_to_new_dir "$NEW_PATH"
-    fi
-done
+url="https://${user}:${pass}@gitlab.sis.ad.bia.itau/homologacion/sap-crm-tests.git"
+#git -c http.sslVerify=false clone -b dev "${url}"
 
 
-# comando para ejecutar el script monitor_and_copy: ./monitor_and_copy.sh > monitor.log 2>&1 &
-# comando para matar todos los procesos vincualdos al script monitor_and_copy: pkill -f monitor_and_copy.sh
+if [ -e "$FULL_PATH" ]; then
+     echo "La carpeta '${FOLDER}' existe en '${DIR}'. Haciendo git pull..."
+     cd 'sap-crm-tests'
+     git config http.sslVerify false
+     git pull
+else
+     echo "La carpeta '$FOLDER' no existe en '$DIR'. Clonando rama dev..."
+     git -c http.sslVerify=false clone -b dev "${url}"
+     git config --global --add safe.directory "${FULL_PATH}"
+ fi
 
---
+++ configs/send_results.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+folder=$1
+job_name=$2
+folder_lower=$(echo "${folder}" | tr '[:upper:]' '[:lower:]')
+
+ALLURE_RESULTS_DIRECTORY="sap-crm-tests/PlaywrightFramework/projects/${folder}/allure-results"
+ALLURE_SERVER="http://testmcil01.sis.ad.bia.itau:5050"
+
+# Project ID according to existent projects in your Allure container - Check endpoint for project creation >> `[POST]/projects`
+PROJECT_ID=${folder_lower}
+echo $folder
+echo $folder_lower
+#Busca si ya existe el proyecto
+response=$(eval curl -X GET "$ALLURE_SERVER/allure-docker-service/projects/search?id=$PROJECT_ID" -H 'accept: */*' | jq -r '.meta_data.message')
+
+echo $response
+if [ "${response}" == 'Project not found' ]; then
+        echo "El proyecto no existe en Allure. Construyendo..."
+        #curl -X 'POST' "$ALLURE_SERVER/allure-docker-service/projects" -H 'accept: */*' -H 'Content-Type: application/json' -d "{'id': $PROJECT_ID}"
+        curl -X 'POST' \
+          "$ALLURE_SERVER/allure-docker-service/projects" \
+          -H 'accept: */*' \
+          -H 'Content-Type: application/json' \
+          -d "{
+          \"id\": \"${PROJECT_ID}\"
+  }"
+
+#curl -X 'POST' 'http://testmcil01.sis.ad.bia.itau:5050/allure-docker-service/projects' -H 'accept: */*' -H 'Content-Type: application/json' -d '{"id": "prueba"}'
+fi
+
+ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ FILES_TO_SEND=$(ls -dp $DIR/$ALLURE_RESULTS_DIRECTORY/* | grep -v /$)
+ if [ -z "$FILES_TO_SEND" ]; then
+   exit 1
+   fi
+
+   FILES=''
+   for FILE in $FILES_TO_SEND; do
+     FILES+="-F files[]=@$FILE "
+     done
+
+     set -o xtrace
+     echo "------------------SEND-RESULTS------------------"
+     curl -X POST "$ALLURE_SERVER/allure-docker-service/send-results?project_id=$PROJECT_ID" -H 'Content-Type: multipart/form-data' $FILES -ik
+
+#
+#
+#     #If you want to generate reports on demand use the endpoint `GET /generate-report` and disable the Automatic Execution >> `CHECK_RESULTS_EVERY_SECONDS: NONE`
+#     #echo "------------------GENERATE-REPORT------------------"
+#     #EXECUTION_NAME='execution_from_my_bash_script'
+#     #EXECUTION_FROM='http://google.com'
+#     #EXECUTION_TYPE='bamboo'
+#
+#     #You can try with a simple curl
+#     #RESPONSE=$(curl -X GET "$ALLURE_SERVER/allure-docker-service/generate-report?project_id=$PROJECT_ID&execution_name=$EXECUTION_NAME&execution_from=$EXECUTION_FROM&execution_type=$EXECUTION_TYPE" $FILES)
+#     #ALLURE_REPORT=$(grep -o '"report_url":"[^"]*' <<< "$RESPONSE" | grep -o '[^"]*$')
+#
+#     #OR You can use JQ to extract json values -> https://stedolan.github.io/jq/download/
+#     #ALLURE_REPORT=$(echo $RESPONSE | jq '.data.report_url')
